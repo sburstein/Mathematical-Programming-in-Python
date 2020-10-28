a12e1c8ab9df8d363a0728ebcf85cf9d56cacc25
Scott Burstein <scott.burstein@duke.edu>
2020-10-28 11:42:58 -0400

    File Reading Issue Resolved

--- File_I:O_Read_Graph.py
+++ File_I:O_Read_Graph.py
@@ -4,73 +4,46 @@ File I/O example
 """
 import numpy as np
 
+#Python list used for adjacency matrix. Sarah Northover and I collaborated.
 
-def write_dfile(x, y, fname):
-    """ writes (x,y) data to a file in two columns"""
-    n = len(x)
-    fp = open(fname, 'w')
-    fp.write(f'{n}\n')
-    for k in range(n):
-        line = "{:.4f}, {:.8f}\n".format(x[k], y[k])
-        fp.write(line)
+def read_graph_data(fname):
 
-    fp.close()
+    adj = {}
+    names = {}
 
-
-def make_example(n, fname):
-    """ Makes the example file for the lecture """
-    x = [5*k/(n-1) for k in range(n)]
-    y = [np.sin(v) for v in x]
-    write_dfile(x, y, fname)
-
-
-def read_dfile(fname):
-    """ Reads in data from write_dfile. Simplest version.
-        This is *not* a safe implementation (to be revisited...)"""
     fp = open(fname, 'r')
-    line = fp.readline()
-    n = int(line)
-    x = [0]*n
-    y = [0]*n
-    k = 0
-    line = fp.readline()
-    while line:  # empty -> False, otherwise true
-        # python knows to strip whitespace, e.g. float('  1.0\n') works
-        words = line.split(',')
-        x[k] = float(words[0])
-        y[k] = float(words[1])
-        line = fp.readline()
-        k += 1
 
-    fp.close()
+    node_prefix = 'n'
+    edge_prefix = 'e'
 
-    return x, y
+    for line in fp:
 
+        comps = line.split(' ')
+        
+        index = int(comps[1])
+        
+        if comps[0][0] == node_prefix: #Data entry represents Node
+            val = comps[2].strip('\n')
+            names[index] = val
+        
+        elif comps[0][0] == edge_prefix: #Data entry represents Edge
+            name = int(comps[2])
+            if index not in adj:
+                adj[index] = [name]
+            else:
+                adj[index].append(name)
 
-def read_dfile2(fname):
-    """ using readlines to get all lines first"""
-    fp = open(fname, 'r')
-    lines = fp.readlines()
-    n = int(lines[0])
-    x = [0]*n
-    y = [0]*n
-    for k in range(1, len(lines)):
-        words = lines[k].split(',')
-        x[k-1] = float(words[0])
-        y[k-1] = float(words[1])
-
-    fp.close()
-    return x, y
+    return adj, names
 
 
-def read_dfile3(fname):
-    """ ... and with all the tricks ..."""
-    with open(fname, 'r') as fp:
-        n = int(fp.readline())
-        x = [0]*n
-        y = [0]*n
-        for k, line in enumerate(fp):  # slick way to iterate over lines of f
-            words = line.split(',')
-            x[k], y[k] = [float(w) for w in words]
+if __name__ == "__main__":  
 
-    return x, y
+    adj_output, names_output = read_graph_data("graph.txt")
+
+    print('adj:  ', adj_output, '\nnames:', names_output)
+
+    #OUPUT:
+    """
+    adj: {0: [1, 3], 1: [2], 2: [1, 0]} 
+    names: {0: 'A', 1: 'B', 2: 'C', 3: 'D'}
+    """
